c27efee5ff1584609bc865e1271460f3f85319f1
aineniamh <aine.otoole@ed.ac.uk>
2024-07-25 14:31:36 +0100

    adding forward muts to the reversion plot

--- squirrel/utils/cns_qc.py
+++ squirrel/utils/cns_qc.py
@@ -153,6 +153,7 @@ def flag_reversions(branch_paths, branch_snp_dict, refs, root_node):
     possible_reversions = []
     branch_reversions = collections.defaultdict(set)
     snp_to_branch = {}
+    will_be_reverted = collections.defaultdict(set)
     for tip in branch_paths:
         path_snps = []
         for branch in branch_paths[tip]:
@@ -163,7 +164,9 @@ def flag_reversions(branch_paths, branch_snp_dict, refs, root_node):
                     base = int(i[0])
                     
                     allele = i[1][-1]
+                    
                     branch_reversions[branch].add(f"{base}{allele}")
+                    will_be_reverted[snp_to_branch[i[0]]].add(f"{base}{i[1][0]}")
                     reversion_to = []
                     
                     ref_alleles = []
@@ -192,15 +195,55 @@ def flag_reversions(branch_paths, branch_snp_dict, refs, root_node):
                         "reversion_to":";".join(reversion_to)
                     })
 
+                
                 snp_to_branch[i[0]] = branch
                 path_snps.append(i)
     if branch_reversions:
         print(green("Reversions flagged:"))
         for i in branch_reversions:
             print(f"- {i} {branch_reversions[i]}")
-    return possible_reversions,branch_reversions
+    return possible_reversions,branch_reversions,will_be_reverted
 
-def make_reversion_tree_figure(outfile,branch_snps,branch_reversions,treefile,w,h):
+
+def flag_convergence(treefile, branch_snp_dict):
+    snp_to_branch = collections.defaultdict(set)
+    branch_convergence = collections.defaultdict(set)
+
+    my_tree=bt.loadNewick(treefile,absoluteTime=False)
+
+    for k in my_tree.Objects:
+        current_node = k
+        if k.branchType == 'leaf':
+            current_node.traits["label"]=k.name
+            node_name = current_node.traits["label"]
+            
+        try:
+            parent_name = current_node.parent.traits["label"]
+            node_name = current_node.traits["label"]
+        except:
+            continue
+        
+        branch = f"{parent_name}_{node_name}"
+
+        snps = branch_snp_dict[branch]
+        for i in snps:
+            snp_to_branch[i].add(branch)
+    convergent_snps= []
+    for snp in snp_to_branch:
+        if len(snp_to_branch[snp])> 1:
+            report_snp = f"{snp[1][0]}{snp[0]}{snp[1][-1]}"
+            convergent_snps.append(report_snp)
+            for branch in snp_to_branch[snp]:
+                branch_convergence[branch].add(report_snp)
+
+    if convergent_snps:
+        print("Convergent snps flagged:")
+        for i in convergent_snps:
+            print(f"- {i}")
+    return branch_convergence
+
+
+def make_reversion_tree_figure(outfile,branch_snps,branch_reversions,will_be_reverted,treefile,w,h):
     
     branch_snps_dict = read_in_branch_snps(branch_snps)
 
@@ -239,12 +282,17 @@ def make_reversion_tree_figure(outfile,branch_snps,branch_reversions,treefile,w,
         if branch_name in branch_snps_dict:
             snps = []
             reversions = []
+            tb_reversions = []
             if branch_name in branch_reversions:
                 for s in branch_reversions[branch_name]:
                     reversions.append(s)
 #                 print(branch_name, len(branch_snps_dict[branch_name]))
+            if branch_name in will_be_reverted:
+                for s in will_be_reverted[branch_name]:
+                    tb_reversions.append(s)
             snp_placement = current_node.parent.height + increment
             rev_placement = (current_node.parent.height + current_node.height)/2
+            tb_rev_placement = (current_node.parent.height + current_node.height)/2
             for s in branch_snps_dict[branch_name]:
                 site,snp,dimer = s
                 if snp == "G->A":
@@ -261,9 +309,14 @@ def make_reversion_tree_figure(outfile,branch_snps,branch_reversions,treefile,w,
                     snps.append((2,"#d9b660"))
             
             for reversion in reversions:
-                plt.text(s=reversion,x=rev_placement,y=k.y+1,rotation="vertical")
+                plt.text(s=reversion,x=rev_placement,y=k.y+0.5,rotation="vertical")
                 plt.scatter([rev_placement],[k.y],color="black",s=150,marker=8)
-                rev_placement += 2*increment
+                rev_placement += 3*increment
+            for tb_reversion in tb_reversions:
+                plt.text(s=tb_reversion,x=tb_rev_placement,y=k.y+0.5,rotation="vertical")
+                plt.scatter([tb_rev_placement],[k.y],color="black",s=150,marker=9)
+                tb_rev_placement += 3*increment
+
             for snp in sorted(snps, key = lambda x : x[0]):
                 plt.scatter([snp_placement],[k.y+0.5],color=snp[1],s=30)
                 snp_placement += increment
@@ -280,18 +333,106 @@ def make_reversion_tree_figure(outfile,branch_snps,branch_reversions,treefile,w,
                    transparent=True)
     # plt.show()
 
-def check_for_reversions_to_reference(state_file, branch_snps, treefile,assembly_refs,mask_file, outfile,outfig):
+def make_convergence_tree_figure(outfile,branch_snps,branch_convergence,treefile,w,h):
+    
+    branch_snps_dict = read_in_branch_snps(branch_snps)
+
+    my_tree=bt.loadNewick(treefile,absoluteTime=False)
+
+    fig,ax = plt.subplots(figsize=(w,h),facecolor='w')
+
+    x_attr=lambda k: k.height ## x coordinate of branches will be absoluteTime attribute
+    su_func=lambda k: 50-30*k.height/my_tree.treeHeight ## size of tips
+    s_func=lambda k: 50-20*k.height/my_tree.treeHeight ## size of tips
+    c_func=lambda k: "dimgrey"
+
+    increment = my_tree.treeHeight/150
+    my_tree.plotTree(ax,x_attr=x_attr) ## plot branches
+    my_tree.plotPoints(ax,size=s_func,colour=c_func,x_attr=x_attr) ## plot circles at tips
+    mpl.rcParams['font.family'] = 'sans-serif'
+
+    target_func=lambda k: k.is_leaf() ## which branches will be annotated
+    text_func=lambda k: k.name ## what text is plotted
+    text_x_attr=lambda k: k.height+(increment*4) ## where x coordinate for text is
+
+    my_tree.addText(ax,x_attr=text_x_attr,target=target_func,text=text_func) #
+    
+    for k in my_tree.Objects:
+        current_node = k
+        if k.branchType == 'leaf':
+            current_node.traits["label"]=k.name
+
+        node_name = current_node.traits["label"]
+        try:
+            parent_name = current_node.parent.traits["label"]
+        except:
+            continue
+        branch_name= f"{parent_name}_{node_name}"
+        
+        if branch_name in branch_snps_dict:
+            snps = []
+            convergent_snps = []
+            if branch_name in branch_convergence:
+                for s in branch_convergence[branch_name]:
+                    convergent_snps.append(s)
+#                 print(branch_name, len(branch_snps_dict[branch_name]))
+
+            snp_placement = current_node.parent.height + increment
+            c_placement = (current_node.parent.height + current_node.height)/2
+            for s in branch_snps_dict[branch_name]:
+                site,snp,dimer = s
+                if snp == "G->A":
+                    if dimer in ["GA"]:
+                        snps.append((1,"#995e62"))
+                    else:
+                        snps.append((2,"#d9b660"))
+                elif snp == "C->T":
+                    if dimer in ["TC"]:
+                        snps.append((1,"#995e62"))
+                    else:
+                        snps.append((2,"#d9b660"))
+                else:
+                    snps.append((2,"#d9b660"))
+            
+            for c_snp in convergent_snps:
+                plt.text(s=c_snp,x=c_placement,y=k.y+0.5,rotation="vertical")
+                plt.scatter([c_placement],[k.y],color="black",s=150,marker="d")
+                c_placement += 2*increment
+
+            for snp in sorted(snps, key = lambda x : x[0]):
+                plt.scatter([snp_placement],[k.y+0.5],color=snp[1],s=30)
+                snp_placement += increment
+
+    [ax.spines[loc].set_visible(False) for loc in ['top','right','left','bottom']]
+    ax.tick_params(axis='y',size=0)
+    ax.tick_params(axis='x',size=0)
+
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+
+    plt.savefig(f"{outfile}.svg")
+    plt.savefig(f"{outfile}.png",bbox_inches='tight', 
+                   transparent=True)
+
+
+def check_for_snp_anomalies(state_file, branch_snps, treefile,assembly_refs,mask_file,out_convergence, outfile,conv_outfig,outfig,h):
     branch_snp_dict = read_in_branch_snps(branch_snps)
     branch_paths= get_path_to_root(treefile)
 
     refs = load_assembly_refs(assembly_refs)
     node1 = get_seq_at_node(state_file,"Node1")
 
-    possible_reversions,branch_reversions = flag_reversions(branch_paths, branch_snp_dict, refs, node1)  
-    
+    possible_reversions,branch_reversions,will_be_reverted = flag_reversions(branch_paths, branch_snp_dict, refs, node1)  
+    branch_convergence = flag_convergence(treefile, branch_snp_dict)
+
+    with open(out_convergence, "w") as fw:
+        fw.write("branch,snp\n")
+        for branch in branch_convergence:
+            for snp in branch_convergence[branch]:
+                fw.write(f"{branch},{snp}\n")
 
     with open(outfile, "w") as fw:
-        writer= csv.DictWriter(fw,lineterminator="\n",fieldnames = ["taxon","site","original_snp",
+        writer= csv.DictWriter(fw,lineterminator="\n",fieldnames = ["taxon","site","original_snp","original_branch",
                                                                     "dinucleotide_context",
                                                                     "reversion_snp","reference_alleles",
                                                                     "root_allele","reversion_to","reversion_branch"
@@ -300,8 +441,8 @@ def check_for_reversions_to_reference(state_file, branch_snps, treefile,assembly
         for i in possible_reversions:
             writer.writerow(i)
             
-    
-    make_reversion_tree_figure(outfig,branch_snps,branch_reversions,treefile,w=25,h=60)
+    make_reversion_tree_figure(outfig,branch_snps,branch_reversions,will_be_reverted,treefile,25,h)
+    make_convergence_tree_figure(conv_outfig,branch_snps,branch_convergence,treefile,25,h)
 
     with open(mask_file,"w") as fw:
         fw.write("Name,Minimum,Maximum\n")
@@ -310,15 +451,5 @@ def check_for_reversions_to_reference(state_file, branch_snps, treefile,assembly
                 site = int(j[:-1])
                 allele = j[-1]
                 fw.write(f"{i} reversion to {allele},{site-1},{site}\n")
-
-
-    
-            
-branch_snps = "/Users/s1680070/repositories/alignHPXV/ref_call_test/qcref_combined.tree.branch_snps.reconstruction.csv"
-state_file = "/Users/s1680070/repositories/alignHPXV/ref_call_test/45artic-covsup70.aln.fasta.state"
-treefile = "/Users/s1680070/repositories/alignHPXV/ref_call_test/45artic-covsup70.aln.fasta.treefile"
-assembly_refs = "/Users/s1680070/repositories/alignHPXV/squirrel/data/ref_seq.fasta"
-outfile = "/Users/s1680070/repositories/alignHPXV/ref_call_test/reversions.csv"
-
 
 
