b0d8694518da1203da5dfa18b0d56b7e194a0967
aineniamh <aine.otoole@ed.ac.uk>
2025-04-09 17:34:21 +0100

    clade assignment functions for initial assignment of clades

---
+++ squirrel/utils/clade_assignment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import os
+from squirrel.utils.log_colours import green,cyan
+from Bio import SeqIO
+import csv
+
+from squirrel.utils.config import *
+
+def parse_paf():
+    pass
+
+
+def read_clades(clade_out, config):
+    pass
