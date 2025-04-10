b0dd48ee9e4e667e6c7d3150e349b384fa7ed0c9
aineniamh <aine.otoole@ed.ac.uk>
2025-04-10 13:47:58 +0100

    assignment functions, adding annotation for input fasta and condensing clade info for pass back to cmd.py

--- squirrel/utils/clade_assignment.py
+++ squirrel/utils/clade_assignment.py
@@ -7,9 +7,41 @@ import csv
 
 from squirrel.utils.config import *
 
-def parse_paf():
-    pass
+def get_clade_dict(panel):
+    clade_dict = {"*":"unassigned"}
+    for record in SeqIO.parse(panel, "fasta"):
+        tokens = record.description.split(" ")
+        clade = [i.split("=")[1] for i in tokens if i.startswith("clade")][0]            
+        clade_dict[record.id] = clade.lower()
+    return clade_dict
 
+def parse_paf(paf_file,panel, clade_out, config):
+    assigned_clade = {}
+    clade_dict = get_clade_dict(panel)
+    with open(paf_file, "r") as f:
+        for line in f:
+            tokens = line.rstrip("\n").split("\t")
+            seq_name = tokens[0]
+            ref_hit = tokens[5]
+            clade = clade_dict[ref_hit]
+            # print("####",seq_name, ref_hit, clade)
 
-def read_clades(clade_out, config):
-    pass
+            assigned_clade[seq_name] = clade
+    return assigned_clade
+
+def annotate_fasta_with_clade(assigned_clade, input_fasta, output_fasta):
+    with open(output_fasta,"w") as fw:
+        for record in SeqIO.parse(input_fasta,"fasta"):
+            clade = assigned_clade[record.id]
+            fw.write(f">{record.description} clade={clade}\n{record.seq}\n")
+
+def condense_clade_info(assigned_clade):
+    clades = set()
+    for i in assigned_clade.values():
+        if i.startswith("cladeii"):
+            clades.add("cladeii")
+        elif i.startswith("cladei"):
+            clades.add("cladei")
+        else:
+            clades.add("unassigned")
+    return {KEY_ASSIGNED_CLADES: list(clades)}
