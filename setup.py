ab20c80b5fe3aa45197b07b4db5abcbbd42a297e
aineniamh <aine.otoole@ed.ac.uk>
2025-11-25 15:10:42 +0000

    mod setup and packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup, find_packages
-import glob
-import os
 
 from squirrel import __version__, _program
 
@@ -14,27 +12,4 @@ setup(name='squirrel',
             'squirrel/scripts/reconstruction.smk',
             'squirrel/scripts/interactive_tree.R'
       ],
-      package_data={"squirrel":["data/*"]},
-      install_requires=[
-            "biopython>=1.70",
-            'tabulate',
-            'baltic',
-            'matplotlib',
-            'mako~=1.3.10',
-            'seaborn',
-            'pandas~=2.3.1',
-            "numpy",
-            'scikit-learn==1.7.1',
-            "PuLP"
-        ],
-      description='Some QUIck Reconstruction to Resolve Evolutionary Links',
-      url='https://github.com/aineniamh/squirrel',
-      author='Aine OToole',
-      author_email='aine.otoole@ed.ac.uk',
-      entry_points="""
-      [console_scripts]
-      {program} = squirrel.command:main
-      """.format(program = _program),
-      include_package_data=True,
-      keywords=[],
-      zip_safe=False)
+      package_data={"squirrel":["data/*"]})
